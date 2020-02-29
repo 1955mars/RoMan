ea332253890a09849d92761d465740f37d5e3e3a
Mansoor Patan <1955mars@gmail.com>
2020-02-28 21:24:15 -0500

    Logging functionality added
    	- Two loggers: core, client
    	- Macros & wrappers for spdlog functions

--- RoMan/src/RoMan.h
+++ RoMan/src/RoMan.h
@@ -3,6 +3,7 @@
 //For use only by RoMan applications
 #include <stdio.h>
 #include "RoMan/Application.h"
+#include "RoMan/Log.h"
 
 //-------Entry Point------------
 #include "RoMan/EntryPoint.h"
---
+++ RoMan/src/RoMan/Log.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace RoMan
+{
+	class ROMAN_API Log
+	{
+	public: 
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+// Core log macros
+
+#define RM_CORE_TRACE(...)    RoMan::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define RM_CORE_INFO(...)     RoMan::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define RM_CORE_WARN(...)     RoMan::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define RM_CORE_ERROR(...)    RoMan::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define RM_CORE_FATAL(...)    RoMan::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Client log macros
+
+#define RM_TRACE(...)    RoMan::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define RM_INFO(...)     RoMan::Log::GetClientLogger()->info(__VA_ARGS__)
+#define RM_WARN(...)     RoMan::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define RM_ERROR(...)    RoMan::Log::GetClientLogger()->error(__VA_ARGS__)
+#define RM_FATAL(...)    RoMan::Log::GetClientLogger()->fatal(__VA_ARGS__)
