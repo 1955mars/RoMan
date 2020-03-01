7ef7a1436bc3d225e767344cba8e84174be8599c
Mansoor Patan <1955mars@gmail.com>
2020-03-01 16:56:06 -0500

    GLFW and window class are added

--- RoMan/src/RoMan/Application.h
+++ RoMan/src/RoMan/Application.h
@@ -2,6 +2,7 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Window.h"
 
 namespace RoMan
 {
@@ -11,6 +12,9 @@ namespace RoMan
 		Application();
 		virtual ~Application();
 		void Run();
+	private:
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
 	};
 
 	//To be defined in client
--- RoMan/src/RoMan/Core.h
+++ RoMan/src/RoMan/Core.h
@@ -10,4 +10,12 @@
 	#error RoMan supports only Windows!
 #endif
 
+#ifdef RM_ENABLE_ASSERTS
+	#define RM_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define RM_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+	#define RM_ASSERT(x, ...)
+	#define RM_CORE_ASSERT(x, ...)
+#endif
+
 #define BIT(x) (1 << x)
