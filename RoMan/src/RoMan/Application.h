fdc4bb89264619e2be66bfa99ba79661876bc37b
Mansoor Patan <1955mars@gmail.com>
2020-03-10 09:57:03 -0400

    GLFW window events dispatch

--- RoMan/src/RoMan/Application.h
+++ RoMan/src/RoMan/Application.h
@@ -2,6 +2,7 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "RoMan/Events/ApplicationEvent.h"
 #include "Window.h"
 
 namespace RoMan
@@ -12,7 +13,11 @@ namespace RoMan
 		Application();
 		virtual ~Application();
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
