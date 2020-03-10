2c3bd914c7f6339d00b5e2bd08f4012ec9870c49
Mansoor Patan <1955mars@gmail.com>
2020-03-10 09:57:03 -0400

    GLFW window events dispatch

--- RoMan/src/RoMan/Application.cpp
+++ RoMan/src/RoMan/Application.cpp
@@ -1,21 +1,24 @@
 #include "rmpch.h"
 #include "Application.h"
 
-#include "RoMan/Events/ApplicationEvent.h"
 #include "RoMan/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace RoMan
 {
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 	}
+
 	void Application::Run()
 	{
 		while (m_Running)
@@ -24,6 +27,20 @@ namespace RoMan
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		RM_CORE_TRACE("{0}", e);
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
 
