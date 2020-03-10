b047e2077db8fda0d0e646d680eca029f43d564f
Mansoor Patan <1955mars@gmail.com>
2020-03-10 17:40:52 -0400

    ImGui Events Added

--- RoMan/src/RoMan/ImGui/ImGuiLayer.h
+++ RoMan/src/RoMan/ImGui/ImGuiLayer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "RoMan/Layer.h"
+#include "RoMan/Events/ApplicationEvent.h"
+#include "RoMan/Events/KeyEvent.h"
+#include "RoMan/Events/MouseEvent.h"
 
 namespace RoMan
 {
@@ -14,6 +17,17 @@ namespace RoMan
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizedEvent(WindowResizeEvent& e);
+
 
 	private:
 		float m_time = 0.0f;
