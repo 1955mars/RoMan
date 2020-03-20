9cc40358ebac5f974e0213e848042d0ccc6b0c9a
Mansoor Patan <1955mars@gmail.com>
2020-03-20 00:05:14 -0400

    RoMan Key codes added

---
+++ RoMan/src/RoMan/KeyCodes.h
@@ -0,0 +1,126 @@
+#pragma once
+
+//From glfw.h
+
+#define RM_KEY_SPACE              32
+#define RM_KEY_APOSTROPHE         39  /* ' */
+#define RM_KEY_COMMA              44  /* , */
+#define RM_KEY_MINUS              45  /* - */
+#define RM_KEY_PERIOD             46  /* . */
+#define RM_KEY_SLASH              47  /* / */
+#define RM_KEY_0                  48
+#define RM_KEY_1                  49
+#define RM_KEY_2                  50
+#define RM_KEY_3                  51
+#define RM_KEY_4                  52
+#define RM_KEY_5                  53
+#define RM_KEY_6                  54
+#define RM_KEY_7                  55
+#define RM_KEY_8                  56
+#define RM_KEY_9                  57
+#define RM_KEY_SEMICOLON          59  /* ; */
+#define RM_KEY_EQUAL              61  /* = */
+#define RM_KEY_A                  65
+#define RM_KEY_B                  66
+#define RM_KEY_C                  67
+#define RM_KEY_D                  68
+#define RM_KEY_E                  69
+#define RM_KEY_F                  70
+#define RM_KEY_G                  71
+#define RM_KEY_H                  72
+#define RM_KEY_I                  73
+#define RM_KEY_J                  74
+#define RM_KEY_K                  75
+#define RM_KEY_L                  76
+#define RM_KEY_M                  77
+#define RM_KEY_N                  78
+#define RM_KEY_O                  79
+#define RM_KEY_P                  80
+#define RM_KEY_Q                  81
+#define RM_KEY_R                  82
+#define RM_KEY_S                  83
+#define RM_KEY_T                  84
+#define RM_KEY_U                  85
+#define RM_KEY_V                  86
+#define RM_KEY_W                  87
+#define RM_KEY_X                  88
+#define RM_KEY_Y                  89
+#define RM_KEY_Z                  90
+#define RM_KEY_LEFT_BRACKET       91  /* [ */
+#define RM_KEY_BACKSLASH          92  /* \ */
+#define RM_KEY_RIGHT_BRACKET      93  /* ] */
+#define RM_KEY_GRAVE_ACCENT       96  /* ` */
+#define RM_KEY_WORLD_1            161 /* non-US #1 */
+#define RM_KEY_WORLD_2            162 /* non-US #2 */
+		
+/* FunctRMkeys */
+#define RM_KEY_ESCAPE             256
+#define RM_KEY_ENTER              257
+#define RM_KEY_TAB                258
+#define RM_KEY_BACKSPACE          259
+#define RM_KEY_INSERT             260
+#define RM_KEY_DELETE             261
+#define RM_KEY_RIGHT              262
+#define RM_KEY_LEFT               263
+#define RM_KEY_DOWN               264
+#define RM_KEY_UP                 265
+#define RM_KEY_PAGE_UP            266
+#define RM_KEY_PAGE_DOWN          267
+#define RM_KEY_HOME               268
+#define RM_KEY_END                269
+#define RM_KEY_CAPS_LOCK          280
+#define RM_KEY_SCROLL_LOCK        281
+#define RM_KEY_NUM_LOCK           282
+#define RM_KEY_PRINT_SCREEN       283
+#define RM_KEY_PAUSE              284
+#define RM_KEY_F1                 290
+#define RM_KEY_F2                 291
+#define RM_KEY_F3                 292
+#define RM_KEY_F4                 293
+#define RM_KEY_F5                 294
+#define RM_KEY_F6                 295
+#define RM_KEY_F7                 296
+#define RM_KEY_F8                 297
+#define RM_KEY_F9                 298
+#define RM_KEY_F10                299
+#define RM_KEY_F11                300
+#define RM_KEY_F12                301
+#define RM_KEY_F13                302
+#define RM_KEY_F14                303
+#define RM_KEY_F15                304
+#define RM_KEY_F16                305
+#define RM_KEY_F17                306
+#define RM_KEY_F18                307
+#define RM_KEY_F19                308
+#define RM_KEY_F20                309
+#define RM_KEY_F21                310
+#define RM_KEY_F22                311
+#define RM_KEY_F23                312
+#define RM_KEY_F24                313
+#define RM_KEY_F25                314
+#define RM_KEY_KP_0               320
+#define RM_KEY_KP_1               321
+#define RM_KEY_KP_2               322
+#define RM_KEY_KP_3               323
+#define RM_KEY_KP_4               324
+#define RM_KEY_KP_5               325
+#define RM_KEY_KP_6               326
+#define RM_KEY_KP_7               327
+#define RM_KEY_KP_8               328
+#define RM_KEY_KP_9               329
+#define RM_KEY_KP_DECIMAL         330
+#define RM_KEY_KP_DIVIDE          331
+#define RM_KEY_KP_MULTIPLY        332
+#define RM_KEY_KP_SUBTRACT        333
+#define RM_KEY_KP_ADD             334
+#define RM_KEY_KP_ENTER           335
+#define RM_KEY_KP_EQUAL           336
+#define RM_KEY_LEFT_SHIFT         340
+#define RM_KEY_LEFT_CONTROL       341
+#define RM_KEY_LEFT_ALT           342
+#define RM_KEY_LEFT_SUPER         343
+#define RM_KEY_RIGHT_SHIFT        344
+#define RM_KEY_RIGHT_CONTROL      345
+#define RM_KEY_RIGHT_ALT          346
+#define RM_KEY_RIGHT_SUPER        347
+#define RM_KEY_MENU               348
