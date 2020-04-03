73dcb8f381f2f05c358750e8deb851f2c14ff71b
Mansoor Patan <1955mars@gmail.com>
2020-04-03 14:05:54 -0400

    Orthographic Camera Added

--- RoMan/src/RoMan/Renderer/Shader.h
+++ RoMan/src/RoMan/Renderer/Shader.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include "glm/glm.hpp"
 
 namespace RoMan
 {
@@ -12,6 +13,8 @@ namespace RoMan
 
 		void Bind() const;
 		void UnBind() const;
+
+		void UploadUniformMatrix4(const std::string& name, const glm::mat4& matrix);
 	private:
 		uint32_t m_RendererID;
 	};
