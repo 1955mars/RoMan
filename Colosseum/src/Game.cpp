3b1c243354202925772e75052c19d5b9caf20745
Mansoor Patan <1955mars@gmail.com>
2020-04-05 23:03:26 -0400

    Abstraction of Shader Class and OpenGLShader created

--- Colosseum/src/Game.cpp
+++ Colosseum/src/Game.cpp
@@ -1,11 +1,13 @@
 #include "rmpch.h"
 #include <RoMan.h>
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
 #include "glm/glm.hpp"
 
 #include "glm/gtc/matrix_transform.hpp"
-
+#include "glm/gtc/type_ptr.hpp"
 
 class ExampleLayer : public RoMan::Layer
 {
@@ -92,9 +94,9 @@ public:
 				color = v_Color;
 			}
 		)";
-		m_Shader.reset(new RoMan::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(RoMan::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
@@ -112,20 +114,22 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
 
+			uniform vec3 u_Color;
+
 			void main()
 			{
-					color = vec4(0.2, 0.3, 0.8, 1.0);
+					color = vec4(u_Color, 1.0);
 			}
 		)";
 
-		m_BlueShader.reset(new RoMan::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(RoMan::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(RoMan::Timestep ts) override
@@ -155,13 +159,17 @@ public:
 		RoMan::Renderer::BeginScene(m_Camera);
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		std::dynamic_pointer_cast<RoMan::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<RoMan::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
 		for (int x = 0; x < 20; x++)
 		{
 			for (int y = 0; y < 20; y++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				RoMan::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				RoMan::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
@@ -172,7 +180,9 @@ public:
 
 	virtual void OnImGuiRender() override
 	{
-
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnEvent(RoMan::Event& event) override
@@ -184,7 +194,7 @@ private:
 	std::shared_ptr<RoMan::Shader> m_Shader;
 	std::shared_ptr<RoMan::VertexArray> m_VertexArray;
 
-	std::shared_ptr<RoMan::Shader> m_BlueShader;
+	std::shared_ptr<RoMan::Shader> m_FlatColorShader;
 	std::shared_ptr<RoMan::VertexArray> m_SquareVA;
 
 	RoMan::OrthographicCamera m_Camera;
@@ -194,6 +204,7 @@ private:
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationspeed = 10.0f;
 
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 
