e75d51e08d20d11dbf23b74dc8c385ecec4d1e05
Mansoor Patan <1955mars@gmail.com>
2020-04-09 01:16:26 -0400

    Multiprocessor compilation flag added in premake

--- premake5.lua
+++ premake5.lua
@@ -8,6 +8,11 @@ workspace "RoMan"
 		"Dist"
 	}
 
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 -- Include directories relative to root folder (solution directory)
