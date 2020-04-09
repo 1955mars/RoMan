778d143defd0e0640a0e3987e07b9e7bf0502366
Mansoor Patan <1955mars@gmail.com>
2020-04-09 02:55:41 -0400

    Shader source code moved into files

--- RoMan/src/RoMan/Renderer/Shader.cpp
+++ RoMan/src/RoMan/Renderer/Shader.cpp
@@ -6,6 +6,24 @@
 
 namespace RoMan
 {
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:
+			RM_CORE_ASSERT(false, "Renderer API is not supported by RoMan Engine");
+			return nullptr;
+
+		case RendererAPI::API::OpenGL:
+			return new OpenGLShader(filepath);
+
+		}
+
+		RM_CORE_ASSERT(false, "Renderer API is not supported by RoMan Engine");
+
+		return nullptr;
+	}
+
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
