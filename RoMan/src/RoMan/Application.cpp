a2b8042cf2708bb92be89a4d3b14980438dd73cc
Mansoor Patan <1955mars@gmail.com>
2020-04-09 00:47:44 -0400

    Blending is supported now. Loading both RGB and RGBA textures

--- RoMan/src/RoMan/Application.cpp
+++ RoMan/src/RoMan/Application.cpp
@@ -24,6 +24,8 @@ namespace RoMan
 		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 
+		Renderer::Init();
+
 		m_ImGuiLayer = new ImGuiLayer();
 		PushOverlay(m_ImGuiLayer);
 
