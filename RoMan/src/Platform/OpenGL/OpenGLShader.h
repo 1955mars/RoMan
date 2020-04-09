f7114ee141be7b377c50768a31e09b60b8885992
Mansoor Patan <1955mars@gmail.com>
2020-04-09 02:55:41 -0400

    Shader source code moved into files

--- RoMan/src/Platform/OpenGL/OpenGLShader.h
+++ RoMan/src/Platform/OpenGL/OpenGLShader.h
@@ -3,12 +3,15 @@
 
 #include "glm/glm.hpp"
 
+//TODO: Remove
+typedef unsigned int GLenum;
 
 namespace RoMan
 {
 	class OpenGLShader : public Shader
 	{
 	public:
+		OpenGLShader(const std::string& filepath);
 		OpenGLShader(const std::string& vertexSrc, const std::string& fragmentSrc);
 		virtual ~OpenGLShader();
 
@@ -24,6 +27,11 @@ namespace RoMan
 
 		void UploadUniformMatrix3(const std::string& name, const glm::mat3& matrix);
 		void UploadUniformMatrix4(const std::string& name, const glm::mat4& matrix);
+
+	private:
+		std::string ReadFile(const std::string& filepath);
+		std::unordered_map<GLenum, std::string> PreProcess(const std::string& source);
+		void Compile(const std::unordered_map<GLenum, std::string>& shaderSources);
 	private:
 		uint32_t m_RendererID;
 	};
